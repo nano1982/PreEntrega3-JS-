541d6fef39971791e100a4ebec126ba5d2aabe50
nano1982 <63853403+nano1982@users.noreply.github.com>
2023-01-23 15:02:41 -0300

    primer commit

---
+++ Usuarios.js
@@ -0,0 +1,14 @@
+class Usuario {
+    nombre;
+    simbolo;
+    contador;
+    empates;
+
+    constructor (nombre, simbolo, contador, empates)
+    {
+        this.nombre = nombre
+        this.simbolo = simbolo
+        this.contador = contador
+        this.empates = empates
+    }
+}
